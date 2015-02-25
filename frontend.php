aa94ff1401025af2d397f9c72e18c3ffcce6adae
Angelito M. Goulart <angelitomgoulart@gmail.com>
2015-02-25 12:39:27 -0300

    contact form function done

--- frontend.php
+++ frontend.php
@@ -34,7 +34,8 @@
     error_reporting(0);
     define('DIRTYCMS_DB_FILE', dirname(__FILE__) . '/db.sqlite');
     define('DIRTYCMS_UPLOADS_PATH', dirname(__FILE__) . '/uploads/');
-    define('DIRTYCMS_CONTACT_EMAIL', 'test@example.com'); 
+    define('DIRTYCMS_FROM_EMAIL', 'yourname@yourdomain.com'); 
+    define('DIRTYCMS_TO_EMAIL', 'yourname@yourdomain.com');
 
     // - Dont touch here
 
@@ -70,15 +71,48 @@
     }
 
     // - Here a contact form function. This must send a mail to address defined on configs.
-    // - Must receive as parameter a array with these fields: 'name', 'telephone', 'email', 'subject', 'message'.
+    // - Must receive as parameter a array with these fields: 'name', 'phone', 'email', 'subject', 'message'.
     // - The returned value is a array with two fields: status, message. The status value is success or danger.
-    // - The message is a custom string which can be modified on contactForm function body.
+    // - The message is a custom string which can be modified on DirtyCMS_contactForm function body.
 
     // Contact form function
 
     function DirtyCMS_contactForm($data){
 
-        // To be implemented...
+        $name    = (isset($data['name']))    ? filter_var($data['name'], FILTER_SANITIZE_STRING)    : '';
+        $phone   = (isset($data['phone']))   ? filter_var($data['phone'], FILTER_SANITIZE_STRING)   : '';
+        $email   = (isset($data['email']))   ? filter_var($data['email'], FILTER_SANITIZE_EMAIL)    : '';
+        $subject = (isset($data['subject'])) ? filter_var($data['subject'], FILTER_SANITIZE_STRING) : '';
+        $message = (isset($data['message'])) ? filter_var($data['message'], FILTER_SANITIZE_STRING) : '';
+
+        if (empty($name) || empty($phone) || empty($email) || empty($message)){
+            $status['type'] = 'danger';
+            $status['message'] = 'Fill all fields correctly.';
+            return $status;
+        }
+
+        $headers =  "MIME-Version: 1.1\r\n";
+        $headers .= "Content-type: text/plain; charset=utf-8\n";
+        $headers .= "Return-Path: " . DIRTYCMS_FROM_EMAIL . "\r\n";
+        $headers .= "From: " . DIRTYCMS_FROM_EMAIL . "\r\n";
+        $headers .= "To: " . DIRTYCMS_TO_EMAIL . "\r\n";
+        $headers .= "Reply-To: {$email}\r\n";
+
+        $body  = "-- {$subject} --\r\n\r\n";
+        $body .= "{$name}\r\n";
+        $body .= "{$phone}\r\n";
+        $body .= "{$email}\r\n";
+        $body .= "\r\n{$message}\r\n";
+        
+        if (mail(DIRTYCMS_TO_EMAIL, $subject, $body, $headers)) {
+            $status['type'] = 'success';
+            $status['message'] = 'Message sent successfully.';
+        } else {
+            $status['type'] = 'danger';
+            $status['message'] = 'Error while sending message. Try again later.';
+        }
+
+        return $status;
 
     }
 
